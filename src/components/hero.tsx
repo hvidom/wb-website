a00e030f2bfbd2e13c1c0f79b1640a28151efcbc
Hryhorii Victorovich Ilin <devopsick@gmail.com>
2026-04-09 18:57:05 +0100

    add

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,44 +1,29 @@
 import { ArrowUpRight, CirclePlay } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import LightRays from './lightrays';
-
-
- 
-
+import { cn } from "@/lib/utils";
+import AnimatedGridPattern from "@/components/ui/animated-grid-pattern";
 
 export default function Hero() {
   return (
-    <section className="relative overflow-hidden">
-      <div className="absolute inset-0 -z-10 pointer-events-none">
-    <div style={{ width: '100%', height: '700px', position: 'relative' }}>
-       <LightRays
-    raysOrigin="top-center"
-    raysColor="#ffffff"
-    raysSpeed={1}
-    lightSpread={0.5}
-    rayLength={3}
-    followMouse={true}
-    mouseInfluence={0.1}
-    noiseAmount={0}
-    distortion={0}
-    className="custom-rays"
-    pulsating={false}
-    fadeDistance={1}
-    saturation={1}
-/>
-</div>
-</div>
-<div className="relative z-10">
-    <div className="flex min-h-screen items-center justify-center px-6">
-      <div className="max-w-3xl text-center">
+    <div className="relative flex min-h-screen items-center justify-center overflow-hidden px-6">
+      <AnimatedGridPattern
+        className={cn(
+          "mask-[radial-gradient(500px_circle_at_center,white,transparent)]",
+          "inset-x-0 h-full skew-y-12"
+        )}
+        duration={3}
+        maxOpacity={0.1}
+        numSquares={30}
+      />
+      <div className="relative z-10 max-w-3xl text-center">
         <Badge
           asChild
           className="rounded-full border-border py-1"
           variant="secondary"
         >
           <a href="#">
-          🎓 3-Tier Development Program<ArrowUpRight className="ml-1 size-4" />
+            🎓 3-Tier Development Program <ArrowUpRight className="ml-1 size-4" />
           </a>
         </Badge>
         <h1 className="mt-6 font-bold text-4xl tracking-tighter sm:text-5xl md:text-6xl md:leading-[1.2] lg:text-7xl">
@@ -49,19 +34,17 @@ export default function Hero() {
         </p>
         <div className="mt-12 flex items-center justify-center gap-4">
           <Button className="rounded-full text-base" size="lg">
-            Explore Programs <ArrowUpRight className="size-5" />
+            Explore Programs <ArrowUpRight className="h-5! w-5!" />
           </Button>
           <Button
             className="rounded-full text-base shadow-none"
             size="lg"
             variant="outline"
           >
-            <CirclePlay className="size-5" /> Book a Consultation
+            <CirclePlay className="h-5! w-5!" /> Book a Consultation
           </Button>
         </div>
       </div>
     </div>
-  </div>
-    </section>
   );
 }
